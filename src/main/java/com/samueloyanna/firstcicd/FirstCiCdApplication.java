f940a5de2ad348ebf380e709200560a5e7f47ca7
SamuelOyanna <39469729+SamuelOyanna@users.noreply.github.com>
2021-07-26 17:21:03 -0400

    First Commit

---
+++ src/main/java/com/samueloyanna/firstcicd/FirstCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.samueloyanna.firstcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samueloyanna/firstcicd/FirstCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueloyanna.firstcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
